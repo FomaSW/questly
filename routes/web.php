e1e86d95e4c7e09f37f8d5ab1d48b6d22391de0f
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-08 01:36:22 +0200

    create project

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use App\Http\Controllers\BotController;
-use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 //Route::post('/' . env('TELEGRAM_BOT_TOKEN') . '/webhook', [BotController::class, 'handleWebhook']);
 //
 
-Route::post('/telegram/webhook', [BotController::class, 'handleWebhook'])
-    ->withoutMiddleware([VerifyCsrfToken::class]);
+//Route::post('/telegram/webhook',[BotController::class, 'handleWebhook']);
