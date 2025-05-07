c92356480389e05d998074de749fe933ab310919
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-08 00:35:50 +0200

    create project

--- database/migrations/2025_05_07_223529_create_tasks_table.php
+++ database/migrations/2025_05_07_223529_create_tasks_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('chat_id')->index();
             $table->string('title');
-            $table->enum('priority', ['високий', 'середній', 'низький'])->default('середній');
+            $table->enum('priority', [1, 2, 3])->default(2);
             $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
@@ -22,4 +25,4 @@ class CreateTasksTable extends Migration
     {
         Schema::dropIfExists('tasks');
     }
-}
+};
