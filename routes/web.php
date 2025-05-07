ce755f4adb52e8c206497419a28c6c87ee03e1a2
Alexandr Yemelianov <fcdnipro268@gmail.com>
2025-05-07 21:49:26 +0200

    create project

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
 
 Route::get('/env-check', function () {
     return [
+        'app_env' => \Illuminate\Support\Env::getRepository(),
         'app_url' => env('APP_URL'),
         'db_host' => env('DB_HOST'),
         'telegram_token' => env('TELEGRAM_BOT_TOKEN'),
